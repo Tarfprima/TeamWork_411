741b0f54d976095ffcac7bcf7cbb6d23bae468d6
Sof Sur <ssurovneva@mail.ru>
2025-08-14 22:08:23 +0500

    создала superuser, зврегестрировала админку

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Artile)
+class ArticleAdmin (admin.ModelAdmin):
+    list_display = ['title', 'text', 'date']
+
+
