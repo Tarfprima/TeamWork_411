44e18a2c7a145e3661da54f497462bdb7e56a5e6
Sof Sur <ssurovneva@mail.ru>
2025-08-14 22:01:37 +0500

    создала БД (каждый должен прописать двнные в собственный pgpass), создала таблицу Article

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Artile(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.CharField(max_length=100000)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+
