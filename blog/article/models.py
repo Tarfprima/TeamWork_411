19413698a79e4e1181fd600e4c6c117d48648add
Tarfprima <granta.150@gmail.com>
2025-08-19 21:03:58 +0300

    Добавлен str в models.py

--- blog/article/models.py
+++ blog/article/models.py
@@ -6,6 +6,9 @@ class Artiсle(models.Model):
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=100000)
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
 
 
 
