74826ddaed015a153c8e9cb80f20e398d6c628a9
Tarfprima <granta.150@gmail.com>
2025-08-19 21:31:25 +0300

    Добавлены urls ссылки для шаблонов.

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from . import views 
+from . import views
 
 
-# name = 'article'
+app_name = 'article'
 
 urlpatterns = [
-    path ('', views.main, name='main'),
-    path ('register/', views.register, name='register'),
-    path ('login/', views.login, name='login'),
+    path('', views.post_list, name='post_list'),
+    path('about/', views.about, name='about'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('posts/new/', views.post_create, name='post_create'),
 ]
