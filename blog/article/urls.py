dd4121f61c5a35776042dec0fc77b0b4fe2d42a6
Uliana1213324 <ulyana.kostylova@gmail.com>
2025-08-21 19:07:22 +0000

    Задача #33 Пути отремонтированы

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -1,28 +1,17 @@
 from django.urls import path
 from . import views 
 
-
-<<<<<<< HEAD
 app_name = 'article'
-
-urlpatterns = [
-    path ('', views.main, name='main'),
-    path ('register/', views.register, name='register'),
-    path ('login/', views.login, name='login'),
-    path ('posts/', views.post_list, name='posts'),
-    path ('new/', views.post_form, name='new'),
-=======
-
-app_name = 'article'
-
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('about/', views.about, name='about'),
+    #path ('', views.main, name='main'),
+    #path('about/', views.about, name='about'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('posts/new/', views.post_create, name='post_create'),
->>>>>>> b05ce96d636ac492de9c171c06a58d5394bee828
+    #path('logout/', views.logout_view, name='logout'),
+    #path('profile/', views.profile, name='profile'),
+    #path('posts/new/', views.post_create, name='post_create'),
+    path('posts/', views.post_list, name='posts'),
+    path('new/', views.post_form, name='new'),
 ]
